e85fb8d8f3943a8aac5258cdc89b060aa6b8849c
Joachim Barthel <jobarthel@gmail.com>
2013-06-14 15:30:59 +0200

    Initial Upload of v 1.1

---
+++ copy_this/modules/jxexcptns/views/admin/de/jxexcptns_lang.php
@@ -0,0 +1,12 @@
+<?php
+$sLangName  = "Deutsch";
+$iLangNr    = 0;
+$aLang = array(
+    'jxexcptns_menu'          => 'Exception Log',
+    'JXEXCPTNS_TITLE'         => 'Exception Log',
+    'JXEXCPTNS_DOWNLOAD'      => 'Download',
+    'JXEXCPTNS_NOCONTENT'     => 'Keine Eintr&auml;ge vorhanden.',
+    'charset'                 => 'ISO-8859-15',
+);
+
+?>
---
+++ copy_this/modules/jxexcptns/views/admin/en/jxexcptns_lang.php
@@ -0,0 +1,12 @@
+<?php
+$sLangName  = "English";
+$iLangNr    = 1;
+$aLang = array(
+    'jxexcptns_menu'          => 'Exception Log',
+    'JXEXCPTNS_TITLE'         => 'Exception Log',
+    'JXEXCPTNS_DOWNLOAD'      => 'Download',
+    'JXEXCPTNS_NOCONTENT'     => 'No entries found.',
+    'charset'                 => 'ISO-8859-15',
+);
+
+?>
